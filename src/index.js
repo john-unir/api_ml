c00dd4f33f031d8ef49635895834c990313daeab
Mauricio Herrera <johnmauricio.herrera214@comunidadunir.net>
2024-06-20 22:01:08 -0500

    Ajustes para aplicacion PWA

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Registrar el service worker
+serviceWorkerRegistration.register();
+
+// Si quieres empezar a medir el rendimiento en tu aplicación, pasa una función
+// para registrar resultados (por ejemplo: reportWebVitals(console.log))
+// o envíalos a un endpoint de análisis. Aprende más: https://bit.ly/CRA-vitals
 reportWebVitals();
